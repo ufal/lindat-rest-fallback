6765b5e1182718bdccf0bc625d2425b2766ccecb
Loganathan Ramasamy <ramasamy@ufal.mff.cuni.cz>
2013-09-13 11:47:33 +0200

    input text is written as it is .

--- applications/expose_cesilko.py
+++ applications/expose_cesilko.py
@@ -40,7 +40,7 @@ class cesilko(plugin):
             text = kwargs[cesilko.api_key_data]
 
             with input_f as fout:
-                fout.write( utils.uni(text).encode('iso-8859-2') )
+                fout.write( text )
                 print 'Written input data to file ' + fout.name
 
             cmd = "%s %s" % (cesilko.tr_script, input_fname_rel)
