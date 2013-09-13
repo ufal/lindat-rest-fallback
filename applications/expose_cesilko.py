749564664da4125b914c23d80e40b7bec6db84fc
Loganathan Ramasamy <ramasamy@ufal.mff.cuni.cz>
2013-09-13 11:53:00 +0200

    changing the input/output encoding to iso-8859-1

--- applications/expose_cesilko.py
+++ applications/expose_cesilko.py
@@ -35,7 +35,7 @@ class cesilko(plugin):
 
         try:
             #(input_f, input_fname_rel) = self._get_temp_file()
-            (input_f, input_fname_rel) = self._get_unique_file(enc='iso-8859-2')
+            (input_f, input_fname_rel) = self._get_unique_file(enc='iso-8859-1')
             expected_output_file_name = input_f.name + ".SK.out"
             text = kwargs[cesilko.api_key_data]
 
@@ -48,12 +48,12 @@ class cesilko(plugin):
             retcode, stdout, stderr = utils.run( cmd )
             output_exists = os.path.exists(expected_output_file_name)
             if 0 == retcode and os.path.exists(expected_output_file_name):
-                with codecs.open(expected_output_file_name, 'rb', 'iso-8859-2') as fin:
+                with codecs.open(expected_output_file_name, 'rb', 'iso-8859-1') as fin:
                     translated_text = fin.read()
-                    translated_text_uni = translated_text.encode('iso-8859-2').decode('utf-8')
+                    #translated_text_uni = translated_text.encode('iso-8859-2').decode('utf-8')
                     return {
                         "input": text,
-                        "result": translated_text_uni
+                        "result": translated_text
                     }
             else:
                 return self._failed( detail="retcode:%d, exists(%s)=%s, stdout=%s, stderr=%s, cmd=%s" % (retcode,  expected_output_file_name, output_exists, stdout, stderr, cmd) )
