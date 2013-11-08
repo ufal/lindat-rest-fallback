f16b33194b56d12bf4a6c69c1d55360c37ad4059
Loganathan Ramasamy <ramasamy@ufal.mff.cuni.cz>
2013-11-08 18:06:07 +0100

    exception is added to throw error messages

--- applications/expose_cesilko.py
+++ applications/expose_cesilko.py
@@ -12,16 +12,17 @@ class cesilko(plugin):
     """
         Cesilko wrapper
     """
-    # ####################
+    # #######################
     # SOFTWARE INFORMATION
-    # ####################
+    # #######################
     # Fill the information about the plugin. Please do not change the following 
     # variable names. 
-    # 1. author   - Author name of the software. Use commas to add more authors.
-    # 2. version  - Software version number.
-    # ####################
+    # 1. software_author   - Author name of the software. Use commas to add more authors.
+    # 2. software_version  - Software version number.
+    # ########################
     software_author = [u'Jan Hajič', u'Vladislav Kuboň', u'Petr Homola']
     software_version = u'v1.0'
+    ##########################
 
     # #########################
     # PLUGIN SPECIFIC VARIABLES
@@ -29,16 +30,18 @@ class cesilko(plugin):
     exposed_uri = "/cesilko"
     temp_dir = "/home/cesilko/tmp/"
     tr_script = "/var/www/cgi-bin/cesilko/run.csh"
+    ###########################
 
-    # ########################
+    # #########################
     # DEFINE API NAMES
-    # ########################
+    # #########################
     # Avilable APIs:
     #   1. translate
     #       data - <UTF-8 encoded string>
-    # ########################
+    # #########################
     api_translate = "translate"
     api_key_data = "data"
+    ###########################
 
     def version(self):
         """ Cesilko version """
@@ -56,17 +59,22 @@ class cesilko(plugin):
         try:
             (input_f, input_fname_rel) = self._get_unique_file(enc='iso-8859-2')
             expected_output_file_name = input_f.name + ".SK.out"
+    
             # 1. Input text is in UTF-8
             text = kwargs[cesilko.api_key_data]
             self.log("Received Input Text: %s ", text)
             self.log("Type of the Input: %s",  str(type(text)))
-            # 2. Convert the text into ISO-8859-2 encoding.
+
+            # 2. Convert the UTF-8 encoded text into ISO-8859-2 encoding.
             #    - non ISO-8859-2 characters will be replaced with XML numeric codes
-            text_iso = text.encode('iso-8859-2', 'xmlcharrefreplace')
-            text_iso_dec = text_iso.decode('iso-8859-2')
-            self.log("Replacing the Non ISO-8859-2 Characters Into XML Numeric Entities: %s", text_iso_dec)
+            try:
+                text_iso = text.encode('iso-8859-2', 'xmlcharrefreplace')
+                text_iso_dec = text_iso.decode('iso-8859-2') # ISO-8859-2 text
+                self.log("Replacing the Non ISO-8859-2 Characters Into XML Numeric Entities: %s", text_iso_dec)
+            except UnicodeEncodeError:
+                self._failed( detail="please supply utf-8 input." )
+
             with input_f as fout:
-                #text_in_iso = unicode(text, errors="ignore").encode('iso-8859-2')
                 fout.write( text_iso_dec )
                 self.log("Written Input Text to File: %s", fout.name)
             cmd = "%s %s" % (cesilko.tr_script, input_fname_rel)
@@ -76,6 +84,7 @@ class cesilko(plugin):
             if 0 == retcode and os.path.exists(expected_output_file_name):
                 with open(expected_output_file_name, 'rb') as fin:
                     translated_text = fin.read()
+                    # convert the ISO-8859-2 output text into UTF-8 text
                     translated_text_dec_utf = translated_text.decode('iso-8859-2').encode('utf-8').decode('utf-8')
                     self.log("Output From Cesilko: %s ", translated_text)
                     self.log("The UTF-8 Encoded Output: %s", translated_text_dec_utf)
@@ -90,10 +99,7 @@ class cesilko(plugin):
             return self._failed( detail=utils.uni(e) )
 
         finally:
-
             fout.close()
-    #
-    #
 
     def _get_temp_file(self):
         """
